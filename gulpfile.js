280a7720facb60faca4d9c9ca26cbeeab490c434
AKD47 <van83horn@gmail.com>
2016-08-04 16:40:04 +0300

    business page load file section

--- gulpfile.js
+++ gulpfile.js
@@ -129,7 +129,7 @@ gulp.task('extend', function () {
 
 });
 
-gulp.task('watch', ['compress'], function() {
+gulp.task('watch', ['compress', 'extend'], function() {
     gulp.watch('app/img/**/*', ['img']);
     gulp.watch('app/sass/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
     gulp.watch(['app/html/*.html'], ['extend']);
